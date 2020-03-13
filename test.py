a13e715183e2574de99885dd5e85559f65635b0f
vince <iwyswx@163.com>
2020-03-13 18:00:36 +0800

    updata

--- test.py
+++
@@ -1,9 +0,0 @@
-#coding:utf-8
-import subprocess
-import pprint
-
-a = subprocess.getoutput('linstor --no-color --no-utf8 n l')
-b = subprocess.getoutput('linstor --no-color --no-utf8 sp l')
-c = subprocess.getoutput('linstor --no-color --no-utf8 r lv')
-pprint.pprint(b)
-pprint.pprint(c)
